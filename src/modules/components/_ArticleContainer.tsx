8d9c7781918eb982f9bdffa6c7db0dde6845335e
Brandon Whittle <brandonwwhittle@gmail.com>
2022-08-05 14:29:17 -0500

    FEAT(): Remove requirement for children

--- src/modules/components/_ArticleContainer.tsx
+++ src/modules/components/_ArticleContainer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 interface ArticleProps {
   title: string
-  children: React.ReactNode
+  children?: React.ReactNode
 }
 
 const ArticleContainer = (props: ArticleProps): JSX.Element => {
