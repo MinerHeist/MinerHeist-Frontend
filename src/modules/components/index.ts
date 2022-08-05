501bb7213f0cf30202c2aa511bb78d2a48da4b50
Brandon Whittle <brandonwwhittle@gmail.com>
2022-08-05 14:21:56 -0500

    FEAT(): Update Landing page example; Correct component exports

--- src/modules/components/index.ts
+++ src/modules/components/index.ts
@@ -1,1 +1,4 @@
+export { default as ArticleContainer } from './_ArticleContainer'
+export { default as Footer } from './_Footer'
+export { default as ImageContainer } from './_ImageContainer'
 export { default as Navbar } from './_Navbar'
--- src/modules/pages/_Landing.tsx
+++ src/modules/pages/_Landing.tsx
@@ -1,7 +1,111 @@
+import { ArticleContainer, ImageContainer } from '../components'
+import alice_1 from '../images/alice_1.jpg'
+import alice_2 from '../images/alice_2.jpg'
+import alice_3 from '../images/alice_3.jpg'
+
 const Landing = (): JSX.Element => {
   return (
     <div className="page landing">
-      <h1>Landing Page</h1>
+      <ArticleContainer title="Landing Page">
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum quidem
+          repellendus nemo similique distinctio doloribus unde sapiente alias
+          consectetur consequatur, voluptatem nisi autem? Suscipit minima amet
+          aliquid earum quisquam assumenda, voluptates iste, cum esse, quasi
+          corrupti! Modi, a hic soluta unde accusantium ut aspernatur autem quam
+          blanditiis sapiente iste magni. Optio a sequi quis quod, placeat neque
+          minus praesentium odit quos velit magnam distinctio provident mollitia
+          accusamus consectetur assumenda esse ipsam vel perspiciatis sapiente
+          voluptatum blanditiis molestias! Iusto tempore numquam reprehenderit.
+          Voluptatum ducimus quos soluta, reiciendis tempore similique dolorem
+          praesentium natus ad, nemo magni est consectetur maiores eveniet ipsam
+          excepturi? Ipsa excepturi voluptatum in inventore, natus quas
+          temporibus nulla, laborum repellendus labore neque aliquid vero
+          reiciendis obcaecati, minima officia laudantium libero illum
+          laboriosam hic saepe ratione culpa. Maiores autem earum saepe
+          assumenda consequuntur vero perferendis totam aliquam obcaecati. Quo a
+          voluptate aut placeat veniam excepturi, inventore quibusdam, dolorem
+          aspernatur, consequatur veritatis. Placeat dignissimos, earum quos ea
+          repellat in provident non optio asperiores repellendus vitae fugit
+          perspiciatis consequuntur, eius omnis. Beatae minus corrupti ducimus
+          dignissimos omnis, eveniet ad dicta veniam qui natus sint ea libero
+          deleniti laboriosam reiciendis molestias quod itaque eaque esse
+          quibusdam excepturi voluptas vel. Quisquam consequuntur laudantium
+          dolor!
+        </p>
+        <ImageContainer
+          url={alice_1}
+          caption="Alice In Wonderland is my favorite example for media."
+        />
+        <ImageContainer url={alice_2} caption="A violent fanart of Alice" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea doloribus
+          sed aspernatur numquam expedita animi, explicabo accusantium
+          doloremque laudantium esse suscipit saepe ut cum, perferendis voluptas
+          officia molestiae. Dignissimos eveniet aperiam explicabo cumque, iure
+          officiis adipisci soluta neque fuga laudantium vel culpa vitae
+          architecto quo, enim facilis quasi commodi necessitatibus inventore
+          possimus. Iure, exercitationem magnam quis, tempora perferendis nam
+          quaerat aperiam reprehenderit obcaecati optio ipsam facere, suscipit
+          dolor ipsum qui maxime? Expedita assumenda, tenetur voluptate
+          quibusdam corporis dignissimos iste consequatur ex possimus ab quo.
+          Facilis optio aliquam nobis quasi nulla suscipit necessitatibus veniam
+          hic! Sapiente quibusdam eligendi necessitatibus, iusto vitae ipsam
+          dicta repudiandae odio assumenda voluptates, animi odit officia
+          recusandae reiciendis nostrum vel voluptas consequuntur earum maiores
+          accusantium rem. Totam ad vitae incidunt unde eaque sit dignissimos
+          non, quia et recusandae tenetur aliquid obcaecati delectus? Ipsum
+          exercitationem sapiente cupiditate ratione. Dolores consequuntur
+          eligendi necessitatibus, rerum laboriosam inventore debitis
+          consectetur totam?
+        </p>
+        <ImageContainer url={alice_3} caption="It might be tea time" />
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi
+          exercitationem temporibus quae pariatur, ipsam fugit? Consequatur
+          pariatur accusamus atque porro vel consectetur explicabo, beatae
+          veritatis repellat obcaecati amet corporis quis voluptatum
+          necessitatibus molestias quo in error iste dolore reprehenderit
+          perferendis deserunt. Dolor adipisci eum suscipit iusto atque
+          cupiditate nisi sapiente nemo praesentium fugit? Dolore sequi deserunt
+          fugiat odio non neque libero officiis atque, molestias vel ipsa, a
+          commodi tenetur est, distinctio nesciunt? Explicabo optio molestiae,
+          sapiente culpa autem deserunt quisquam mollitia eum hic totam libero
+          at inventore omnis, est modi nihil quas nostrum temporibus nobis
+          similique quibusdam in. Sequi non optio repellendus quia, magni ea
+          nostrum temporibus unde ratione perspiciatis nam ducimus corrupti
+          cupiditate labore quis, consequuntur, enim laborum. Nostrum
+          exercitationem vero magnam, aliquam et delectus obcaecati facere
+          distinctio laborum quam quia. Impedit voluptas delectus, veniam
+          laudantium non, eaque voluptatem quia fugit natus voluptate tempore
+          ipsam sint praesentium perferendis officiis ducimus, maiores iste?
+          Reiciendis cumque accusamus odit assumenda provident ab ad porro
+          incidunt quae voluptatum, sed, voluptas ullam, iste ex nesciunt. In
+          quasi fuga consectetur provident ratione! Sunt sit voluptatum dolores
+          quidem id, doloribus ad aliquid. Quaerat suscipit ad recusandae,
+          corporis dignissimos nobis accusantium laboriosam incidunt ratione
+          quidem voluptates, debitis aliquid ut quod quam! Doloremque tempore
+          rem ratione, molestias fugiat, ea possimus voluptatibus numquam ullam
+          voluptas nemo voluptate. Accusantium ipsum quibusdam reiciendis
+          consectetur, voluptas illo eos consequatur dicta. Est modi autem neque
+          aut eaque molestias, veritatis esse sapiente dolorem, aliquam ut
+          laudantium tenetur sunt cumque assumenda? Quam magnam at veritatis.
+        </p>
+        <br />
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore
+          mollitia omnis porro earum, nemo error necessitatibus molestiae soluta
+          veniam eum tempora eligendi in eius magni! Temporibus, repudiandae
+          dolorem incidunt facere consequatur sed praesentium, ea ratione
+          provident facilis ipsa sit eaque beatae veritatis illo optio minima
+          explicabo aperiam, deleniti iusto quisquam quidem eos qui voluptates.
+          Amet ad atque exercitationem repellendus quibusdam, veritatis ut
+          voluptatem culpa nihil placeat mollitia minus cumque odio facere in
+          repudiandae iure rem voluptate a ipsa minima debitis eveniet sapiente.
+          Asperiores, illum maiores iusto ut dolorem, quisquam odit cum adipisci
+          nisi nobis mollitia cupiditate officiis. Debitis, fugiat recusandae?
+        </p>
+      </ArticleContainer>
     </div>
   )
 }
